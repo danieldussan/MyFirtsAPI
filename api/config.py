9736140c6b04d2889c74c984606b1a8da527b9dc
danieldussan <daniel2014dussan@gmail.com>
2024-07-19 14:57:52 -0500

    cambio para vercel API

--- api/config.py
+++ api/config.py
@@ -1,11 +1,14 @@
+import os
+import dotenv
+dotenv.load_dotenv()
 class DevelopmentConfig():
     DEBUG = True
     
     # Mysql Connection 
-    MYSQL_HOST = 'localhost' 
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'esp'
+    MYSQL_HOST = os.getenv('MYSQL_HOST') 
+    MYSQL_USER = os.getenv('MYSQL_USER') 
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD') 
+    MYSQL_DB = os.getenv('MYSQL_DB') 
 
 Config = {
     'Development' :  DevelopmentConfig
